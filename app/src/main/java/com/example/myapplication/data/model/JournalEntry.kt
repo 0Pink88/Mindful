e186c642f3c1b5d3662bf027c6c171c2cc42a19c
Silencieux <ajbd10@gmail.com>
2026-04-05 22:52:03 -0500

    Added text screen files for journal. Set up journal screen to dynamically change depending on entry list.

--- app/src/main/java/com/example/myapplication/data/model/JournalEntry.kt
+++ app/src/main/java/com/example/myapplication/data/model/JournalEntry.kt
@@ -3,7 +3,7 @@ package com.example.myapplication.data.model
 import android.R
 
 data class JournalEntry(
-    val id: Long = 0,
+    val id: Int = 0,
     val date: String = "",
     val content: String = "",
     val complete: Boolean = false
--- app/src/main/java/com/example/myapplication/ui/text/TextScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/text/TextScreen.kt
@@ -2,28 +2,26 @@ package com.example.myapplication.ui.text
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.myapplication.ui.journal.JournalViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextScreen(navController: NavController) {
+fun TextScreen(navController: NavController, viewModel: JournalViewModel = viewModel()) {
 
+    //main column and back button
     Column(modifier = Modifier.fillMaxSize().padding(16.dp)) { //via MoodScreen.kt
         TopAppBar(
             title = { Text("Journal") },
