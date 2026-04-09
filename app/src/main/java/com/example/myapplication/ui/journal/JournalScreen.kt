06e8d3d923c6db2ee1969a4c08dc6c37b5c65489
Silencieux <ajbd10@gmail.com>
2026-04-08 23:44:58 -0500

    Added Text viewing functionality for journal entries. Adjusted formatting to reflect wireframes. Reordered existing journal logic into separate functions.

--- app/src/main/java/com/example/myapplication/ui/journal/JournalScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/journal/JournalScreen.kt
@@ -1,18 +1,18 @@
 package com.example.myapplication.ui.journal
 
-import android.R.attr.entries
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.BottomAppBar
@@ -22,7 +22,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
@@ -33,16 +32,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.myapplication.data.model.JournalEntry
 
+//still need to add:
+//Add entry functionality
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun JournalScreen(navController: NavController, viewModel: JournalViewModel = viewModel()) {
+
+    val journalEntries = viewModel.journalEntries
+    var selectedEntry by remember { mutableStateOf<JournalEntry?>(null)}
 
     //new journal bar
     Scaffold(
@@ -52,9 +56,11 @@ fun JournalScreen(navController: NavController, viewModel: JournalViewModel = vi
                     .wrapContentSize()
                     .padding(16.dp)
             ) {
-                Button(onClick = {navController.navigate("text") }, modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(4.dp) ) {
+                Button(
+                    onClick = { navController.navigate("text") }, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(4.dp)
+                ) {
                     Text("Add Entry")
                 }
             }
@@ -62,9 +68,11 @@ fun JournalScreen(navController: NavController, viewModel: JournalViewModel = vi
     ) {}
 
     //main column with back button
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp)) { //via MoodScreen.kt
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {                                 //via MoodScreen.kt
         TopAppBar(
             title = { Text("Journal") },
             navigationIcon = {
@@ -73,30 +81,48 @@ fun JournalScreen(navController: NavController, viewModel: JournalViewModel = vi
                 }
             }
         )
-        Text("Journal Entries")
-        Spacer(modifier = Modifier.height(12.dp))
 
-        val journalEntries = viewModel.journalEntries
-
-        // journal grid
+        // journal row
         //dynamically changes depending on journalEntries list
-        //will change layout later
-        LazyVerticalGrid(
-            modifier = Modifier.wrapContentSize(Alignment.TopCenter),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            columns = GridCells.Fixed(3)
-        ) {
-            items(journalEntries) { item ->
-                ElevatedButton(
-                    onClick = { navController.navigate("text") },
-                    shape = CircleShape
-                ) {
-                    Text(item.date)
-                }
+        JournalRow( items = journalEntries, onItemClick = { item -> selectedEntry = item })
+
+        //checks if entry has been selected, opens entry text if clicked
+        selectedEntry?.let { entry -> OpenText(entry)}
+
+    }
+
+
+
+}
+
+//function that controls journal row of entries
+@Composable
+fun JournalRow( items: List<JournalEntry>, onItemClick: (JournalEntry) -> Unit)
+{
+    LazyRow(modifier = Modifier.wrapContentSize(Alignment.TopCenter),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+        items(items) { item ->
+            ElevatedButton(
+                onClick = { onItemClick(item) },
+                shape = CircleShape
+            ) {
+                Text(item.date)
             }
         }
     }
+}
+
+//function that controls the formatting for opened uneditable text
+@Composable
+fun OpenText(item : JournalEntry) {
+
+    val scroll = rememberScrollState()
+    Column () {
+        Text(item.content,
+            modifier = Modifier.height(650.dp).verticalScroll(scroll).offset(x = 11.dp),
+            textAlign = TextAlign.Justify)
+    }
+
 }
 
 
