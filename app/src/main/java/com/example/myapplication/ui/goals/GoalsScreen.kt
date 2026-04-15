b1a266729f844e743139869acc022b348e578b6e
Moseley, Jake <jakemoseley@my.unt.edu>
2026-04-15 15:51:11 -0500

    Added back button to GoalScreen.kt

--- app/src/main/java/com/example/myapplication/ui/goals/GoalsScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/goals/GoalsScreen.kt
@@ -1,16 +1,32 @@
 package com.example.myapplication.ui.goals
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GoalsScreen(navController: NavController) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Goals Screen")
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.White).padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        TopAppBar(title = {Text("")}, navigationIcon = { IconButton(onClick = {navController.popBackStack()})  {
+            Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+        }})
+
     }
 }
