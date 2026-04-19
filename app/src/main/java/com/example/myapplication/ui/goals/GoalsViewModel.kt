4b91a6fe84aba58a36563a8468eabc8c4426f75c
Moseley, Jake <jake66888@gmail.com>
2026-04-19 15:19:45 -0500

    Added goal functionality to view model

--- app/src/main/java/com/example/myapplication/ui/goals/GoalsViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/goals/GoalsViewModel.kt
@@ -1,6 +1,26 @@
 package com.example.myapplication.ui.goals
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.myapplication.data.model.Goal
 
 class GoalsViewModel : ViewModel() {
+    var goals by mutableStateOf(
+        listOf(
+            Goal(1, "Say hi to alex", false),
+            Goal(2, "Drink water", false),
+            Goal(3, "Walk Around", false)
+        )
+    )
+        private set
+    fun toggleGoal(id: Int) {
+        goals = goals.map {
+            if (it.id == id) it.copy(isCompleted = !it.isCompleted)
+            else it
+        }
+    }
+    val completedGoals: Int
+        get() = goals.count {it.isCompleted}
 }
